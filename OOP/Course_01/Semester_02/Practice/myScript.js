f2fc4fbd9353e39f044ef5311cabe8abafdfb8e6
Ametov Imil <charlz_klug@mail.ru>
2016-07-21 17:30:28 +0300

    Summer practice. Web site.

--- OOP/Course_01/Semester_02/Practice/myScript.js
+++ OOP/Course_01/Semester_02/Practice/myScript.js
@@ -1,4 +1,4 @@
-var numLetters = [], firstMul, secondMul;
+var numLetters = [], firstMul, secondMul, firstResult, secondResult;
 var isOpenFirstMul = []; // Элементы слева-направо
 var isOpenSecondMul = [];
 var isOpenFirstResult = [];
@@ -6,7 +6,13 @@ var isOpenSecondResult = [];
 var isOpenResult = [];
 
 genLetters();
-//genFirstMul();
+
+function checkWin() {
+    var allOpen = true;
+    for (i = 0; i < 3; ++i) allOpen = allOpen && isOpenFirstMul[i];
+    for (i = 0; i < 2; ++i) allOpen = allOpen && isOpenSecondMul[i];
+    return allOpen;
+}
 
 function checkArray(someArray, someNumber, arrayLength, arrayNumber) {
     for (i = 0; i < arrayLength; ++i){
@@ -15,6 +21,78 @@ function checkArray(someArray, someNumber, arrayLength, arrayNumber) {
 	    document.getElementById('letter' + arrayNumber + (i + 1)).innerHTML = someNumber;
 	}
     }
+}
+
+function fillTable() {
+    isOpenFirstMul = [false, false, false];
+    isOpenSecondMul = [false, false];
+    isOpenFirstResult = [false, false, false, true];
+    isOpenSecondResult = [false, false, false, true];
+    isOpenResult = [false, false, false, false, true];
+    var t;
+
+    // firstMul
+    t = Math.floor(Math.random() * 10);
+    document.getElementById('letter11').innerHTML = numLetters[t];
+    firstMul = t;
+    t = Math.floor(Math.random() * 10);
+    document.getElementById('letter12').innerHTML = numLetters[t];
+    firstMul += t * 10;
+    t = Math.floor(Math.random() * 9) + 1;
+    document.getElementById('letter13').innerHTML = numLetters[t];
+    firstMul += t * 100;
+
+    // secondMul
+    t = Math.floor(Math.random() * 10);
+    document.getElementById('letter21').innerHTML = numLetters[t];
+    secondMul = t;
+
+    // Заполнение первого ряда умножения столбиком
+    var k;
+    firstResult = firstMul * t;
+    if (firstResult > 999) {
+	isOpenFirstResult[3] = false;
+	k = 4;
+    }
+    else k = 3;
+    for (i = 1; i <= k; ++i) {
+	document.getElementById('letter3'+i).innerHTML = numLetters[firstResult % 10];
+	firstResult = Math.floor(firstResult / 10);
+    }
+
+    // Генерация второго множителя
+    t = Math.floor(Math.random() * 9) + 1;
+    document.getElementById('letter22').innerHTML = numLetters[t];
+    secondMul += t * 10;
+
+    // Заполнение второго ряда умножения столбиком
+    secondResult = firstMul * t;
+    if (secondResult > 999) {
+	isOpenSecondResult[3] = false;
+	k = 4;
+    }
+    else k = 3;
+    for (i = 1; i <= k; ++i) {
+	document.getElementById('letter4'+i).innerHTML = numLetters[secondResult % 10];
+	secondResult = Math.floor(secondResult / 10);
+    }
+    
+    // Обработка результата умножения
+    temp = firstMul * secondMul;
+    if (temp > 9999) {
+	isOpenFirstResult[4] = false;
+	k = 5;
+    }
+    else k = 4;
+    for (i = 1; i <= k; ++i) {
+	document.getElementById('letter5'+i).innerHTML = numLetters[temp % 10];
+	temp = Math.floor(temp / 10);
+    }
+    console.log(firstMul);
+    console.log(secondMul);
+    
+    
+    
 }
 
 function getNumber(someArray, id) {
@@ -29,8 +107,15 @@ function getNumber(someArray, id) {
 	    checkArray(isOpenFirstResult, someNumber, 4, 3);
 	    checkArray(isOpenSecondResult, someNumber, 4, 4);
 	    checkArray(isOpenResult, someNumber, 5, 5);
+	    if (checkWin() == true) {
+		openTable();
+		if (confirm("Вы победили! Начать новую игру?") == true) fillTable();
+	    }
 	}
     }
+}
+
+function openTable() {
     
 }
 
@@ -78,73 +163,3 @@ function genLetters() {
     numLetters[9] = letters[0];
 }
 
-function fillTable() {
-    isOpenFirstMul = [false, false, false];
-    isOpenSecondMul = [false, false];
-    isOpenFirstResult = [false, false, false, true];
-    isOpenSecondResult = [false, false, false, true];
-    isOpenResult = [false, false, false, false, true];
-    var t;
-
-    // firstMul
-    t = Math.floor(Math.random() * 10);
-    document.getElementById('letter11').innerHTML = numLetters[t];
-    firstMul = t;
-    t = Math.floor(Math.random() * 10);
-    document.getElementById('letter12').innerHTML = numLetters[t];
-    firstMul += t * 10;
-    t = Math.floor(Math.random() * 9) + 1;
-    document.getElementById('letter13').innerHTML = numLetters[t];
-    firstMul += t * 100;
-
-    // secondMul
-    t = Math.floor(Math.random() * 10);
-    document.getElementById('letter21').innerHTML = numLetters[t];
-    secondMul = t;
-
-    // Заполнение первого ряда умножения столбиком
-    var k, temp = firstMul * t;
-    if (temp > 999) {
-	isOpenFirstResult[3] = false;
-	k = 4;
-    }
-    else k = 3;
-    for (i = 1; i <= k; ++i) {
-	document.getElementById('letter3'+i).innerHTML = numLetters[temp % 10];
-	temp = Math.floor(temp / 10);
-    }
-
-    // Генерация второго множителя
-    t = Math.floor(Math.random() * 9) + 1;
-    document.getElementById('letter22').innerHTML = numLetters[t];
-    secondMul += t * 10;
-
-    // Заполнение второго ряда умножения столбиком
-    temp = firstMul * t;
-    if (temp > 999) {
-	isOpenSecondResult[3] = false;
-	k = 4;
-    }
-    else k = 3;
-    for (i = 1; i <= k; ++i) {
-	document.getElementById('letter4'+i).innerHTML = numLetters[temp % 10];
-	temp = Math.floor(temp / 10);
-    }
-    
-    // Обработка результата умножения
-    temp = firstMul * secondMul;
-    if (temp > 9999) {
-	isOpenFirstResult[4] = false;
-	k = 5;
-    }
-    else k = 4;
-    for (i = 1; i <= k; ++i) {
-	document.getElementById('letter5'+i).innerHTML = numLetters[temp % 10];
-	temp = Math.floor(temp / 10);
-    }
-    console.log(firstMul);
-    console.log(secondMul);
-    
-    
-    
-}
