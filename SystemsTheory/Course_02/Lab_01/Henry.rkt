91986dcc3f2fe974a2cc923e88208c6c07df9c17
Ametov Imil <charlz_klug@mail.ru>
2016-12-07 20:28:56 +0300

    Work on Lab 01

--- SystemsTheory/Course_02/Lab_01/Henry.rkt
+++ SystemsTheory/Course_02/Lab_01/Henry.rkt
@@ -2,4 +2,26 @@
 (require math/array)
 
 (define (make-round bx)
-  (make-array (vector bx) 5))
+  (make-array (vector bx) 1))
+
+(define a 
+  (array #[#[1 0 3 2]
+           #[2 0 3 1]
+           #[3 0 2 1]
+           #[3 1 2 0]]))
+
+(define b 
+  (array #[#[0 3 2 1]
+           #[0 1 2 3]
+           #[3 1 0 2]
+           #[2 1 0 3]]))
+
+(define solution
+  (array #[0 0 3 2]))
+
+(define (present-element? el arr ind)
+    (if (< ind (array-size arr))
+        (if (= (array-ref arr (vector ind)) el) 
+            true
+            (present-element? el arr (+ ind 1)))
+        false))
