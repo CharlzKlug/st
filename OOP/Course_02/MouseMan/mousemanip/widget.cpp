62eebe890aab8cefceb8864855e81e955ca79b6d
Ametov Imil <charlz_klug@mail.ru>
2017-06-02 18:27:46 +0300

    OOP. MouseMan modified.

--- OOP/Course_02/MouseMan/mousemanip/widget.cpp
+++ OOP/Course_02/MouseMan/mousemanip/widget.cpp
@@ -8,6 +8,16 @@ Widget::Widget(QWidget *parent)
     //points = new QPoint[pointsCount];
     manhattanDistance = 10;
     capturedPoint = -1;
+
+    // Добавляем координаты экрана в corePoints
+    corePoints = new QPoint[4];
+    corePoints[0] = QPoint(width(),0);
+    corePoints[1] = QPoint(0, 0);
+    corePoints[2] = QPoint(0, height());
+    corePoints[3] = QPoint(width(), height());
+    corePointsCount = 4;
+    coreCentMass = centerPoint(corePoints, corePointsCount);
+    sortByAngle(corePoints, corePointsCount, coreCentMass);
 }
 
 Widget::~Widget()
@@ -15,29 +25,93 @@ Widget::~Widget()
 
 }
 
-void Widget::paintEvent(QPaintEvent *){
+void Widget::myDraw()
+{
+    /*
     QPainter painter(this);
     painter.setPen(Qt::blue);
-    //    painter.setFont(QFont("Arial", 30));
-    //    painter.drawText(rect(), Qt::AlignCenter, "Qt");
     painter.setBrush(Qt::black);
-    //painter.drawEllipse(100, 100, 5, 5);
     painter.setFont(QFont("Arial", 13));
-    //painter.drawText(100, 100, QString::number(x));
-    //    painter.drawEllipse(x, y, 5, 5);
-    //    painter.drawText(x, y, QString::number(x)+" "+QString::number(y));
     for (int i=0; i<pointsCount; i++){
         painter.drawEllipse(points[i], 5, 5);
         painter.drawText(points[i], QString::number(i));
         if (i>0) painter.drawLine(points[i], points[i-1]);
     }
+    if (pointsCount !=0)
+        painter.drawLine(points[0], points[pointsCount-1]);
+    */
+
+
+    QPainter painter(this);
+    painter.setPen(Qt::blue);
+    painter.setBrush(Qt::red);
+    painter.setFont(QFont("Arial", 13));
+    //QPolygon myPoly;
+    //myPoly.setPoints(5, 0,0, 100, 0, 50, 50, 10, 30, 110, 30);
+    //QPainterPath tmpPath;
+    //tmpPath.addPolygon(myPoly);
+    //painter.drawPolygon(myPoly);
+    //painter->fillPath(tmpPath);
+
+    /*
+    QPolygon somePolygon;
+    somePolygon.append(QPoint(0,0));
+    somePolygon.append(QPoint(0, 100));
+    somePolygon.append(QPoint(100, 100));
+    somePolygon.append(QPoint(300, 300));
+    somePolygon.append(QPoint(100, 0));
+
+
+    painter.drawPolygon(somePolygon);
+    */
+    //painter.drawPolygon(points, pointsCount);
+
+    //painter.drawPolygon(corePoints, 4);
+
+    for (int i=0; i<pointsCount; i++){
+        painter.drawEllipse(points[i], 5, 5);
+        /*
+        painter.drawText(points[i], QString::number(i)+" "+
+                         QString::number(cosBetween(points[i])) + " " +
+                         QString::number(vectProduct(points[i]))
+                         );
+                         */
+        if (i<pointsCount-1) painter.drawLine(points[i], points[i+1]);
+        painter.drawText(points[i], QString::number(i) + " " +
+                         QString::number(getAngle(points[i], centPoint)));
+    /*
+        if (i-2>-1) painter.drawLine(points[i], points[i-2]);
+        else if (i>0) painter.drawLine(points[i], points[i-1]);
+        */
+    }
+    if (pointsCount>2)
+        painter.drawLine(points[pointsCount-1], points[0]);
+
+    painter.drawEllipse(centPoint, 2, 2);
+    painter.drawText(centPoint, "c");
+
+    if (pointsCount>2)
+    {
+    buildCore();
+    painter.drawPolygon(corePoints, corePointsCount);
+    }
+    /*
+    if (pointsCount !=0)
+        painter.drawLine(points[0], points[pointsCount-1]);
+    */
+}
+
+void Widget::paintEvent(QPaintEvent *){
+    myDraw();
 }
 
 void Widget::mouseMoveEvent(QMouseEvent * event){
     if (capturedPoint != -1){
         points[capturedPoint] = QPoint(event->x(), event->y());
+        centPoint = centerPoint(points, pointsCount);
         update();
     }
+    if (capturedPoint == -1) sortByAngle(points, pointsCount, centPoint);
 }
 
 void Widget::mousePressEvent(QMouseEvent * event){
@@ -104,9 +178,148 @@ void Widget::mousePressEvent(QMouseEvent * event){
         }
     }
     //this->setWindowTitle("Test... " + myString);
+    centPoint = centerPoint(points, pointsCount);
+    sortByAngle(points, pointsCount, centPoint);
     update();
 }
 
 void Widget::mouseReleaseEvent(QMouseEvent *){
     capturedPoint = -1;
+    sortByAngle(points, pointsCount, centPoint);
+    update();
+}
+
+QPoint Widget::centerPoint(QPoint* arr, int total)
+{
+    double xc, yc;
+    xc = 0;
+    yc = 0;
+    for (int i=0; i<total; i++)
+    {
+        xc += arr[i].rx();
+        yc += arr[i].ry();
+    }
+    xc /= total;
+    yc /= total;
+    return QPoint(xc, yc);
+}
+
+double Widget::cosBetween(QPoint A, QPoint B)
+{
+    // Косинус угла между двумя векторами
+    double sP = A.rx()*B.rx() + A.ry()*B.ry();
+    double delimiter = sqrt((A.rx()*A.rx()+A.ry()*A.ry())*(B.rx()*B.rx()+B.ry()*B.ry()));
+    if (delimiter == 0) return 0;
+    return sP/delimiter;
+}
+
+int Widget::vectProduct(QPoint A, QPoint B)
+{
+    // Векторное произведение вектора A на вектор B
+    //return somePoint.ry() - centPoint.ry();
+    return A.rx()*B.ry() - B.rx()*A.ry();
+}
+
+double Widget::getAngle(QPoint somePoint, QPoint massPoint)
+{
+    double tempCos = cosBetween(somePoint-massPoint, QPoint(1,0));
+    //int tempVect = vectProduct(somePoint);
+    //if (tempVect<0) return arccos(tempCos);
+    if (vectProduct(QPoint(1,0), somePoint-centPoint)>0) return 3.14+acos(-tempCos);
+    return acos(tempCos);
+}
+
+void Widget::sortByAngle(QPoint* arr, int totalPoints, QPoint massPoint)
+{
+    int minIndex;
+    QPoint tempPoint;
+    for (int i=0; i<totalPoints-1; i++)
+    {
+        minIndex=i;
+        for (int j=i+1; j<totalPoints; j++)
+            if (getAngle(arr[minIndex], massPoint)>getAngle(arr[j], massPoint))
+                minIndex=j;
+        if (minIndex != i)
+        {
+            tempPoint=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=tempPoint;
+        }
+    }
+}
+
+void Widget::buildCore()
+{ // Построение ядра
+
+    if (pointsCount>2)
+    {
+    delete [] corePoints;
+    corePoints = new QPoint[4];
+    corePoints[0] = QPoint(width(),0);
+    corePoints[1] = QPoint(0, 0);
+    corePoints[2] = QPoint(0, height());
+    corePoints[3] = QPoint(width(), height());
+    corePointsCount = 4;
+
+    //corePoints = new
+
+    for (int i=0; i<pointsCount; i++)
+    {
+        if (i==pointsCount-1) cutCore(points[i], points[0]);
+        else cutCore(points[i], points[i+1]);
+    }
+}
+}
+
+void Widget::cutCore(QPoint A, QPoint B)
+{
+    QPoint v = B - A;
+    int nextPoint;
+    QPoint vc;
+    double ax, ay, bx, by, sax, say, sbx, sby, delimiter, x, y, proportion;
+    QVector <QPoint> intersections;
+    ax = v.rx();
+    ay = v.ry();
+    sax = A.rx();
+    say = A.ry();
+    for (int i=0; i<corePointsCount; i++)
+    {
+        if (i==corePointsCount-1) nextPoint = 0;
+        else nextPoint = i+1;
+        vc = corePoints[nextPoint] - corePoints[i];
+        bx = vc.rx();
+        by = vc.ry();
+        sbx = corePoints[i].rx();
+        sby = corePoints[i].ry();
+        delimiter = ay*bx - by*ax;
+        if (delimiter != 0)
+        {
+            x = ((sby-say)*ax*bx - sbx*by*ax + sax*ay*bx)/delimiter;
+            y = ((sbx-sax)*ay*by - sby*bx*ay + say*ax*by)/(-delimiter);
+            proportion = (x - sbx)/bx;
+            double proportiony = (y - sby)/by;
+            if ((proportion>=0 && proportion<=1) || (proportiony>=0 && proportiony<=1))
+                intersections.append(QPoint(x, y));
+        }
+    }
+
+    // Проходим по точкам ядра и где векторное произведение меньше
+    // либо равно нулю заносим в intersections
+    for (int i=0; i<corePointsCount; i++)
+    {
+        if (vectProduct(v, corePoints[i] - A) <= 0)
+            intersections.append(QPoint(corePoints[i].rx(), corePoints[i].ry()));
+    }
+
+    // Уничтожаем corePoints и вносим туда содержимое intersections
+    delete [] corePoints;
+    corePointsCount = intersections.size();
+    corePoints = new QPoint[corePointsCount];
+    for (int i=0; i<corePointsCount; i++)
+        corePoints[i] = QPoint(intersections[i].rx(), intersections[i].ry());
+
+    // Сортируем corePoints
+    coreCentMass = centerPoint(corePoints, corePointsCount);
+    sortByAngle(corePoints, corePointsCount, coreCentMass);
+
 }
--- OOP/Course_02/MouseMan/mousemanip/widget.h
+++ OOP/Course_02/MouseMan/mousemanip/widget.h
@@ -5,14 +5,28 @@
 #include <QPainter>
 #include <QMouseEvent>
 #include <QString>
+#include <cmath>
+//#include <QTMath>
 
 class Widget : public QWidget
 {
     Q_OBJECT
 private:
-    int x, y, pointsCount, manhattanDistance, capturedPoint;
+    int x, y, pointsCount,
+    manhattanDistance, capturedPoint, corePointsCount
+    ;
     QString myString;
+    QPoint centPoint;
     QPoint* points;
+    QPoint* corePoints;
+    QPoint coreCentMass; // Центр масс ядра
+    QPoint centerPoint(QPoint*, int);
+    double cosBetween(QPoint, QPoint);
+    int vectProduct(QPoint, QPoint);
+    double getAngle(QPoint, QPoint); // Угол между центром масс и некоторой точкой
+    void sortByAngle(QPoint*, int, QPoint);
+    void buildCore(); // Построение ядра
+    void cutCore(QPoint, QPoint); // Обрезание четырёхугольника
 public:
     Widget(QWidget *parent = 0);
     ~Widget();
@@ -20,6 +34,7 @@ public:
     void mouseMoveEvent(QMouseEvent *);
     void mousePressEvent(QMouseEvent *);
     void mouseReleaseEvent(QMouseEvent *);
+    void myDraw();
 };
 
 #endif // WIDGET_H
