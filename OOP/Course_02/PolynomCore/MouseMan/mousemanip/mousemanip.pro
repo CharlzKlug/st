fd98b0534214ff29c1cce7032c13c4a97f863808
Ametov Imil <charlz_klug@mail.ru>
2017-06-19 15:59:11 +0300

    OOP. PolynomCore.

---
+++ OOP/Course_02/PolynomCore/MouseMan/mousemanip/mousemanip.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-03-14T11:39:15
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = mousemanip
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
---
+++ OOP/Course_02/PolynomCore/MouseMan/mousemanip/widget.h
@@ -0,0 +1,24 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QString>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+private:
+    int x, y, pointsCount, manhattanDistance;
+    QString myString;
+    QPoint* points;
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+    void paintEvent(QPaintEvent *);
+    void mouseMoveEvent(QMouseEvent *);
+    void mousePressEvent(QMouseEvent *);
+};
+
+#endif // WIDGET_H
